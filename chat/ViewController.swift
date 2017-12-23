0011934d66206d61c68ab606d8543be00195eb1b
NIPUN KANADE <nipunkanade@nipun-macbook-pro.local>
2017-12-23 17:59:09 +0530

    Initial Commit

---
+++ chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by NIPUN KANADE on 23/12/17.
+//  Copyright © 2017 Bakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
